b7ce566711f896a1e18aa522efd36f6d5065d0b2
Jishnu <jishnu.nb@neoito.com>
2022-05-03 16:05:22 +0530

    api sdk install configurations changed

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+import fs from "fs";
+import path from "path";
+import { createFileSync, sendResponse } from "./utils.js";
 
+import { env } from "node-blox-sdk";
+env.init();
 
-const f_read_candidates = async (req, res) => {
+/**
+ * List candidates request hanlder
+ * @param {*} req
+ * @param {*} res
+ */
+const listCandidates = (req, res) => {
+  try {
 
-  // health check
-  if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    if (req.params["health"] === "health") {
+      return sendResponse(res, 200, {
+        success: true,
+        msg: "Health check success",
+      });
+    }
+
+    const DB_FILE = path.resolve("../localdb.json");
+    createFileSync(DB_FILE);
+    const data = fs.readFileSync(DB_FILE, { encoding: "utf8", flag: "r" });
+    const resData = JSON.parse(data || "[]");
+    sendResponse(res, 200, { status: true, data: resData, ENV: process.env });
+  } catch (e) {
+    sendResponse(res, 500, { status: false, msg: e.message, err: e });
   }
-
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello f_read_candidates`}))
-  res.end()
-  
-}
-
-export default f_read_candidates
+};
+export default listCandidates;
